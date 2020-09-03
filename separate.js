5cf82d29e6df8c4a275c5e21dfb48a680b1acdd2
nesez <68465610+nesez@users.noreply.github.com>
2020-09-03 20:27:29 +0900

    Update separate.js

--- separate.js
+++ separate.js
@@ -9,7 +9,7 @@
 							var lang = lang.substr(0,2)
 							
 
-							if (lang == "want country")
+							if (lang == "language code")    // check in "https://ko.wikipedia.org/wiki/ISO_639-1_%EC%BD%94%EB%93%9C_%EB%AA%A9%EB%A1%9D"
 								window.location.replace('')
 
 							else
