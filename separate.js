cddbc7891d59ae9fab784bb52d1021c88845cc93
nesez <68465610+nesez@users.noreply.github.com>
2020-09-03 20:25:17 +0900

    Create separate.js

---
+++ separate.js
@@ -0,0 +1,16 @@
+              var type = navigator.appName
+							var lang;
+							if (type=="Netscape")
+								lang = navigator.language
+							else
+								lang = navigator.userLanguage
+							
+
+							var lang = lang.substr(0,2)
+							
+
+							if (lang == "want country")
+								window.location.replace('')
+
+							else
+								window.location.replace('')
